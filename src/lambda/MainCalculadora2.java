061a96a753a7e0a06f22622555b1ed42b0340450
Bruno Rogério <b.rogerio2007@gmail.com>
2023-11-19 19:14:47 -0400

    Iniciando pratica com git

--- src/lambda/MainCalculadora2.java
+++ src/lambda/MainCalculadora2.java
@@ -15,9 +15,8 @@ public class MainCalculadora2 {
         };
         System.out.println(calcMulti.executar(15, 5));
 
-        Calculo calcDiv = (a, b) -> {
-            return a / b;
-        };
+        Calculo calcDiv = (a, b) -> a / b;
+
         System.out.println(calcDiv.executar(10, 10));
 
         Calculo calcSub = (x, y) -> x - y;
