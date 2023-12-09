c32148a9645686c4e02f903497c788fff5ee1e19
Bruno Rogério <b.rogerio2007@gmail.com>
2023-12-09 00:16:57 -0400

    Iniciando pratica com git

--- src/lambda/Produto.java
+++ src/lambda/Produto.java
@@ -1,12 +1,13 @@
 package lambda;
 
-public class Produto {
+public class Produto extends Object {
 
     final String nome;
     final Double preco;
     final Double desconto;
 
     public Produto(String nome, Double preco, Double desconto) {
+        super();
         this.nome = nome;
         this.preco = preco;
         this.desconto = desconto;
---
+++ src/lambda/TestePratica.java
@@ -0,0 +1,14 @@
+package lambda;
+
+import java.util.function.Predicate;
+
+public class TestePratica {
+
+    public static void main(String[] args) {
+
+        Produto p7 = new Produto("MacBook M1", 6000.00, 0.10);
+
+        Predicate<Produto> isCaro = x -> (x.preco * (1 - x.desconto)) > 6500.00;
+        System.out.println(isCaro.test(p7));
+    }
+}
