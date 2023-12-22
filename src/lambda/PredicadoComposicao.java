dc5d9621eacd3fbc11bee5171d9496854cbadc55
Bruno Rogério <b.rogerio2007@gmail.com>
2023-12-21 23:09:42 -0400

    Iniciando pratica com git

---
+++ src/lambda/PredicadoComposicao.java
@@ -0,0 +1,20 @@
+package lambda;
+
+import java.util.function.Predicate;
+
+public class PredicadoComposicao {
+
+    public static void main(String[] args) {
+
+        Predicate<Integer> isPar = num
+                -> num % 2 == 0;
+
+        Predicate<Integer> isTresDigitos =
+                num -> num >= 100 && num < 1000;
+
+        System.out.println(isPar.test(17));
+
+        System.out.println(isPar.and(isTresDigitos).test(144));
+
+    }
+}
