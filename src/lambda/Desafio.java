d020be7118fb8b510484e9b3f5630d5f13fcb43f
Bruno Rogério <b.rogerio2007@gmail.com>
2024-01-03 11:43:47 -0400

    Iniciando pratica com git

--- src/lambda/Desafio.java
+++ src/lambda/Desafio.java
@@ -1,9 +1,6 @@
 package lambda;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.text.DecimalFormat;
-import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public class Desafio {
@@ -12,13 +9,23 @@ public class Desafio {
 
         Produtos p1 = new Produtos("Casa", 400000.00, 0.25);
 
-        Produtos p2 = new Produtos("Lexus",300000.50,0.15);
+        Produtos p2 = new Produtos("Lexus", 300000.50, 0.15);
 
         Double valor = p1.getPreco();
 
+        p2.calcularPrecoComDesconto();
+        p2.calcularImpostoMunicipal();
+        p2.calcularFrete(p2.getPreco());
+
+        System.out.println(p2.getPreco());
+        Double valor2 = p2.calcularPrecoComDesconto() + p2.calcularImpostoMunicipal() + p2.calcularFrete(p2.getPreco());
+        System.out.println(valor2);
+
         Supplier<String> finalTotal = () -> "R$ " + formatarValor(valor);
+        Supplier<String> finalTotal2 = () -> "R$ " + formatarValor(valor2);
 
         System.out.println("O valor final é " + finalTotal.get());  // Exibe R$ 400000,00
+        System.out.println("O valor final é " + finalTotal2.get());
 
     }
 
